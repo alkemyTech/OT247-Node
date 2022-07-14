dafd5f0f4ee772b2ec7f9323ed0dc1f923739b59
Rodrigo Conejeros <rconejerosnavea@gmail.com>
2022-07-14 16:44:48 -0400

    Added controller, migration, model for News. categoryId reference commented until category exists

---
+++ controllers/controller-new.js
@@ -0,0 +1,7 @@
+const {New} = require('../models/new.js');
+
+module.exports = () => {
+	class Newcontroller extends New{
+		
+	}
+}
---
+++ models/new.js
@@ -0,0 +1,27 @@
+'use strict';
+const {  Model} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class New extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      New.belongsTo(models.Category, {as: 'category'});
+    }
+  };
+  New.init({
+    name: DataTypes.STRING,
+    content: DataTypes.STRING,
+    image: DataTypes.STRING,
+    categoryId: DataTypes.INTEGER,
+    deletedAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'New',
+    paranoid: true
+  });
+
+  return New;
+};
