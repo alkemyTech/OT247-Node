5b0c406b3ace38c97a4e655948d6f01087f793f1
Nicolas Preiti <nicolaspreiti45@gmail.com>
2022-07-15 17:58:30 -0300

    delete user endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const { deleteUserController } = require('../controllers/users');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+//DELETE delete user by id
+router.delete('/:id', deleteUserController);
 
 module.exports = router;
