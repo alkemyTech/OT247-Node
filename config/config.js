636386e2841c18e7903bb20882d2e44ac02ff59e
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-07-15 11:15:21 -0300

    sendgrid integration

--- config/config.js
+++ config/config.js
@@ -7,20 +7,26 @@ module.exports = {
         "database":  process.env.DB_NAME,
         "host": process.env.DB_HOST,
         "port": process.env.DB_PORT,
-        "dialect": "mysql"
+        "dialect": "mysql",
+        "sendgridKey": process.env.SENDGRID_API_KEY,
+        "senderEmail": process.env.SENDGRID_SENDER_EMAIL
     },
     "test": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_NAME,
         "host": process.env.DB_HOST,
-        "dialect": "mysql"
+        "dialect": "mysql",
+        "sendgridKey": process.env.SENDGRID_API_KEY,
+        "senderEmail": process.env.SENDGRID_SENDER_EMAIL
     },
     "production": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_NAME,
         "host": process.env.DB_HOST,
-        "dialect": "mysql"
+        "dialect": "mysql",
+        "sendgridKey": process.env.SENDGRID_API_KEY,
+        "senderEmail": process.env.SENDGRID_SENDER_EMAIL
     }
 }
