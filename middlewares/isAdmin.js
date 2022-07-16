1b6b84aba00e1959583bf45f279a340a182bf2b1
Alexis Correa <alexis.correa026@gmail.com>
2022-07-16 18:01:42 -0300

    Inserted return

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -3,7 +3,7 @@ const createHttpError = require('http-errors')
 exports.isAdmin = async (req, res, next) => {
   try {
     const { roleId } = req.body.response
-    if (roleId !== 2) res.status(401).send('[Unauthorized - User] - [Access - Denied]')
+    if (roleId !== 2) return res.status(401).send('[Unauthorized - User] - [Access - Denied]')
     next()
   } catch (error) {
     const httpError = createHttpError(
