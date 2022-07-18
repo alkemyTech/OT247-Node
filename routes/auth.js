27de48fa3368b6eb53774e83484a50a3ec0c4ddb
Alexis Correa <alexis.correa026@gmail.com>
2022-07-18 13:34:12 -0300

    Change the name of the imported function, from post to userRegister

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-const { post } = require('../controllers/users')
+const { userRegister } = require('../controllers/users')
 const { schemaValidator } = require('../middlewares/validator')
 const { user } = require('../schemas/user')
 
-router.post("/register", schemaValidator(user), post)
+router.post("/register", schemaValidator(user), userRegister)
 
 module.exports = router;
