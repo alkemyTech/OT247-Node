edd4772eb3963f9577eeeea5226103ba6934f54a
Javier Alejandro Oyarzo <javi_14_228@hotmail.com>
2022-07-18 23:17:09 -0300

    Fix: identation typo in code

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -4,11 +4,11 @@ const jwt = require('jsonwebtoken');
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 
 const verify = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decoded = jwt.verify(token, accessTokenSecret);
-        req.user = decoded;
-        next();
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, accessTokenSecret);
+      req.user = decoded;
+      next();
     } catch (err) {
         res.status(403).json({
             message: 'Invalid token'
