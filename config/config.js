c63d8bfa622387fdadad961d9788c110c5edef4e
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-07-19 10:31:39 -0300

    environment config

--- config/config.js
+++ config/config.js
@@ -1,29 +1,16 @@
-require('dotenv').config()
+const dotenv = require('dotenv')
 
-const env = process.env.NODE_ENV || 'development'
+dotenv.config({
+  path: `${__dirname}/../.env.${process.env.NODE_ENV}`
+})
 
-if (env === 'production') {
-    module.exports = {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database":  process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "port": process.env.DB_PORT,
-        "dialect": "mysql",
-        "sendgridKey": process.env.SENDGRID_API_KEY,
-        "senderEmail": process.env.SENDGRID_SENDER_EMAIL
-    }
-}
-
-if (env === 'development') {
-    module.exports = {
-        "username": process.env.DB_USER_DEV,
-        "password": process.env.DB_PASSWORD_DEV,
-        "database":  process.env.DB_NAME_DEV,
-        "host": process.env.DB_HOST_DEV,
-        "port": process.env.DB_PORT_DEV,
-        "dialect": "mysql",
-        "sendgridKey": process.env.SENDGRID_API_KEY_DEV,
-        "senderEmail": process.env.SENDGRID_SENDER_EMAIL_DEV
-    }
+module.exports = {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database":  process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": "mysql",
+    "sendgridKey": process.env.SENDGRID_API_KEY,
+    "senderEmail": process.env.SENDGRID_SENDER_EMAIL
 }
