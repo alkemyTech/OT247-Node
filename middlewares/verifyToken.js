89ff09574a23809b358e5f51be82f5df7821f2d2
Javier Alejandro Oyarzo <javi_14_228@hotmail.com>
2022-07-18 22:07:12 -0300

    Add verification token Middleware

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+
+const verify = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, accessTokenSecret);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(403).json({
+            message: 'Invalid token'
+        });
+    };
+};
+
+module.exports = { verify };
