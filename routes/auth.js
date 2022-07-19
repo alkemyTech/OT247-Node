79af4e44ca6ee905136d769de7f5edc17787e7b1
Alexis Correa <alexis.correa026@gmail.com>
2022-07-18 22:16:54 -0300

    Added return in the helper catchAsync

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const { userRegister } = require('../controllers/users')
 const { schemaValidator } = require('../middlewares/validator')
