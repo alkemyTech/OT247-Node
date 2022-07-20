7cfc79b4abee0dbc8372faa1a38123f9da63361f
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-07-20 18:46:32 -0300

    [add] email sending

--- controllers/users.js
+++ controllers/users.js
@@ -3,11 +3,15 @@ const bcrypt = require('bcrypt');
 const createHttpError = require('http-errors')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
+const welcomeMail = require('../mail-templates/mail-templates')
 
 const {
   registerUser,
 } = require('../services/user')
 
+const {
+  sendMail,
+} = require('../services/sendgrid')
 
 module.exports = {
   userRegister: catchAsync(async (req, res, next) => {
@@ -25,6 +29,12 @@ module.exports = {
             body: users,
           }) 
         
+          sendMail({
+            email: body.email,
+            subject: 'Welcome to the app',
+            template: welcomeMail(user),
+            templateId: 'd-4792e3fb740e47ad94ced288fdaf98f8'
+          })
         } catch (error) {
           const httpError = createHttpError(
             error.statusCode,
