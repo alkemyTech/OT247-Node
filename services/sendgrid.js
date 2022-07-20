b4b53baf8e54c7da68fb1f4fd0c013daf8bedde3
rodrigoacn <107000658+rodrigoacn@users.noreply.github.com>
2022-07-20 16:29:08 -0400

    Update with suggested changes by Santiago

--- services/sendgrid.js
+++ services/sendgrid.js
@@ -1,27 +1,24 @@
 const { sendgridKey, senderEmail } = require(__dirname + '/../config/config')
-const { userController } = require(__dirname + '/../controllers/users');
+const { mailWelcomeTemplate } = require(__dirname + '/../mail-templates/mail-welcome-template');
 const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(sendgridKey)
 
-const sendMail = async ({ email, subject, sandboxMode = false }) => {
+const sendMail = async ({ templateId, user, email, subject, sandboxMode = false }) => {
+  const welcomeMailData = mailWelcomeTemplate.welcomeMail(user)
+	const msg = {
+    to: email,
+    from: senderEmail,
+    subject: subject,
+    mail_settings: {
+      sandbox_mode: {
+        enable: sandboxMode
+      }
+    },
+    templateId: templateId,
+    dynamic_template_data: welcomeMailData
+  }
   try {
-  	const user = await userController.findUserByEmail(email)
-  	const msg = {
-	    to: email,
-	    from: senderEmail,
-	    subject: subject,
-	    mail_settings: {
-	      sandbox_mode: {
-	        enable: sandboxMode
-	      }
-	    },
-	    templateId: 'd-9c3c32b4875941d68e2bdae96442c108',
-	    dynamic_template_data: {
-		    name: user.firstName,
-		    city: user.lastName
-		  }
-	  }
     await sgMail.send(msg)
 
   } catch (err) {
