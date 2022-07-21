7a419493445b6bd633bb46f6b98f9cbd44bed44c
Nicolas Preiti <nicolaspreiti45@gmail.com>
2022-07-21 19:54:13 -0300

    route patch to update organization by admin

--- routes/organizations.js
+++ routes/organizations.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { getPublicOrganization } = require('../controllers/organization');
+
+const { isAdmin } = require('../middlewares/isAdmin');
+const { getPublicOrganization, updatePublicOrganization } = require('../controllers/organization');
+const { schemaValidator } = require('../middlewares/validator');
+const { organizationUpdate } = require('../schemas/organization');
 
 //GET public organization
 router.get('/public/:id', getPublicOrganization);
+
+//PATCH update public data of an organization
+router.patch('/public/:id', isAdmin, schemaValidator(organizationUpdate), updatePublicOrganization);
 
 module.exports = router;
