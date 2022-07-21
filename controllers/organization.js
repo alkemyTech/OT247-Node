958c5c9a4c9c63affc6890ce51cd6178e6df7abc
Nicolas Preiti <nicolaspreiti45@gmail.com>
2022-07-21 20:04:29 -0300

    controller and service to update an organization

--- controllers/organization.js
+++ controllers/organization.js
@@ -1,4 +1,4 @@
-const { getPublicOrgService } = require('../services/organization');
+const { getPublicOrgService, updateOrganization } = require('../services/organization');
 
 module.exports = {
   getPublicOrganization: async (req, res) => {
@@ -14,5 +14,28 @@ module.exports = {
 
     //Found organization
     res.status(200).json(organization);
+  },
+  updatePublicOrganization: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      //Check that param id is a integer
+      if (!Number.isInteger(Number(id)))
+        return res.status(412).json({ status: 412, message: 'Id param must be a integer' });
+
+      //Check that req.body is not empty
+      if (Object.keys(req.body) == 0)
+        return res.status(412).json({ status: 412, message: 'Empty fields, nothing to update' });
+
+      //Try to update an organization
+      const updatedOrganization = await updateOrganization(id, req.body);
+
+      //Server Responses
+      updatedOrganization == 0
+        ? res.status(404).json({ status: 404, message: 'Organization not found' })
+        : res.status(200).json({ status: 200, message: 'Organization data updated' });
+    } catch (err) {
+      res.status(400).json({ status: 400, error: 'An error has occurred' });
+    }
   },
 };
