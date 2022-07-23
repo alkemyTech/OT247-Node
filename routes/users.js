a96d4cee083f4b7dca9a361f6d12b486677c06bc
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2022-07-23 17:31:37 -0300

    Fix: Get All Users route with isAdmin Middleware

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const { deleteUserById, getUsers } = require('../controllers/users');
+const { isAdmin } = require('../middlewares/isAdmin');
 
 /* GET users listing. */
-router.get('/', isAdmin, getUsers);
+router.get('/',  isAdmin, getUsers);
 
 //DELETE delete user
 router.delete('/:id', deleteUserById);
