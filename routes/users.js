6f5e17f0879dc5a9616b7570bcc4af8060f98601
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2022-07-23 16:09:01 -0300

    Add: Get All Users Route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const { deleteUserById } = require('../controllers/users');
+const { deleteUserById, getUsers } = require('../controllers/users');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', isAdmin, getUsers);
 
 //DELETE delete user
 router.delete('/:id', deleteUserById);
+
+
 
 module.exports = router;
