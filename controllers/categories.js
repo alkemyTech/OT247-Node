ae072b59f7caf834c392746ae0158dd3d656e889
Nicolas Preiti <nicolaspreiti45@gmail.com>
2022-07-22 23:31:26 -0300

    getCategoryAsAdmin service and controller

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,12 +1,28 @@
-const {Category} = require('../models');
+const { Category } = require('../models');
+const { getCategoryAsAdmin } = require('../services/category');
 
 module.exports = {
-    getCategories: async(req, res, next) => {
-        try{
-            const allCategories = await Category.findAll();
-            res.status(200).json(allCategories);
-        }catch(err){
-            next(err)
-        }
+  getCategories: async (req, res, next) => {
+    try {
+      const allCategories = await Category.findAll();
+      res.status(200).json(allCategories);
+    } catch (err) {
+      next(err);
     }
-}
+  },
+  getCategoryAsAdmin: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      //Try to get a category
+      const gottenCategory = await getCategoryAsAdmin(id);
+
+      //Server responses
+      !gottenCategory
+        ? res.status(404).json({ status: 404, message: 'Category not found' })
+        : res.status(200).json({ status: 200, message: 'Category found', data: gottenCategory });
+    } catch (err) {
+      res.status(400).json({ status: 400, error: 'An error has occurred' });
+    }
+  },
+};
