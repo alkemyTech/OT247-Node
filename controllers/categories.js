9c3034b21406c16f47e63687fd461e2fc7264722
Alexis Correa <alexis.correa026@gmail.com>
2022-07-22 22:25:46 -0300

    [ENDPOINT] OT247-44 Created controller updateCategoryById

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,4 +1,10 @@
-const {Category} = require('../models');
+const { Category } = require('../models');
+
+const createHttpError = require('http-errors')
+const { endpointResponse } = require('../helpers/success')
+const { catchAsync } = require('../helpers/catchAsync')
+
+const { updateCategoryById } = require('../services/category')
 
 module.exports = {
     getCategories: async(req, res, next) => {
@@ -8,5 +14,28 @@ module.exports = {
         }catch(err){
             next(err)
         }
-    }
+    },
+    updateCategoryById: catchAsync(async (req, res, next) => {
+        try {
+        const { id } = req.params;
+        const integerId = parseInt(id, 10);
+        const { body } = req;
+        
+    
+          const category = await updateCategoryById(id, body)
+          endpointResponse({
+            res,
+            message: 'Category updated successfully',
+            body: category,
+          }) 
+        
+          
+        } catch (error) {
+          const httpError = createHttpError(
+            error.statusCode,
+            `[Error updating category] - [Category - POST]: ${error.message}`,
+          );
+          next(httpError) 
+        }
+      })
 }
