b9014caecc6b7f63c02c8a135063a2a1baa8e76d
Alexis Correa <alexis.correa026@gmail.com>
2022-07-22 22:28:05 -0300

    [ENDPOINT] OT247-44 Created route PUT for updating category

--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const {getCategories} = require('../controllers/categories');
+
+const { getCategories, updateCategoryById } = require('../controllers/categories');
+const { isAdmin } = require('../middlewares/isAdmin');
 
 router.get('/', getCategories)
+router.put('/:id', isAdmin, updateCategoryById)
 
 module.exports = router;
 
