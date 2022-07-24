6e8272bfff696566215ec07217a75fd6b890a80d
Nicolas Preiti <nicolaspreiti45@gmail.com>
2022-07-24 12:34:32 -0300

    news controller and service created

--- controllers/news.js
+++ controllers/news.js
@@ -1,6 +1,17 @@
-const {News} = require('../models/new.js');
+const { createNews } = require('../services/news');
 
-module.exports = () => {
-	class Newscontroller extends News{
-	}
-}
+module.exports = {
+  createNews: async (req, res) => {
+    try {
+      const { body } = req;
+
+      //Try to create a news
+      const createdNews = await createNews(body);
+
+      //Server responses
+      res.status(200).json({ status: 200, message: 'News created', data: createdNews });
+    } catch (err) {
+      res.status(400).json({ status: 400, message: 'An has occurred', error: err.message });
+    }
+  },
+};
