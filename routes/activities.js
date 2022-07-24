ff9b0cc9232fb9af1aec5b0386a39323ba595c18
Rodrigo Conejeros <rconejerosnavea@gmail.com>
2022-07-24 17:29:49 -0400

    Now validates if is admin

--- routes/activities.js
+++ routes/activities.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const { activitiesRegister } = require('../controllers/activities');
 
-router.post('/', activitiesRegister);
+router.post('/', isAdmin, function(req, res, next){
+  activitiesRegister(req, res, next);
+});
 
 module.exports = router;
