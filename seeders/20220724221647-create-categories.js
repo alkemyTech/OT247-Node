ea2727339a2819e8b30479cbb40051cc715e3757
Rodrigo Conejeros <rconejerosnavea@gmail.com>
2022-07-24 18:19:58 -0400

    Created with sequelize-cli and added function recomended by Javier

--- seeders/20220724221647-create-categories.js
+++ seeders/20220724221647-create-categories.js
@@ -18,11 +18,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Categories', null, {});
   }
 };
