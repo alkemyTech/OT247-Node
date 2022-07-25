a3b7999b15151fe242f9eb94cd648e0e290b50a6
Alexis Correa <alexis.correa026@gmail.com>
2022-07-25 15:31:32 -0300

    Return response directly

--- helpers/existCategory.js
+++ helpers/existCategory.js
@@ -4,9 +4,6 @@ const { ErrorObject } = require('../helpers/error')
 const existCategory = async(idCategory) => {
     try{
         const category = await Category.findByPk(idCategory);
-        if(!category){
-            return false;
-        }
         return category;
     }catch(err){
         console.log(err);
