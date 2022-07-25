aef0bd36c20cf6a8185e81fe87fa541f056d8e0e
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-07-25 18:07:43 -0300

    [add] delete category route

--- routes/categories.js
+++ routes/categories.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const { isAdmin } = require('../middlewares/isAdmin')
+const { verify } = require('../middlewares/verifyToken')
 const categoryCtrl = require('../controllers/categories');
 const { schemaValidator } = require('../middlewares/validator')
 const { category } = require('../schemas/category')
-const { isAdmin } = require('../middlewares/isAdmin')
-const { verify } = require('../middlewares/verifyToken')
+
+const { categoryExists } = require('../middlewares/categoryExists')
 
 router
   .get('/', categoryCtrl.getCategories)
-  .post('/', verify, isAdmin, schemaValidator(category), categoryCtrl.createCategory)
+  .post('/', isAdmin, schemaValidator(category), categoryCtrl.createCategory)
+  .delete('/:id', isAdmin, categoryExists, categoryCtrl.deleteCategoryById)
 
 module.exports = router;
