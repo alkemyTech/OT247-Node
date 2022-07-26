a6e1612b7eec89c63b901a5d6a515748e270d0e1
Nicolas Preiti <nicolaspreiti45@gmail.com>
2022-07-26 03:51:00 -0300

    emptyBody middleware added in the route

--- routes/organizations.js
+++ routes/organizations.js
@@ -5,11 +5,18 @@ const { isAdmin } = require('../middlewares/isAdmin');
 const { getPublicOrganization, updatePublicOrganization } = require('../controllers/organization');
 const { schemaValidator } = require('../middlewares/validator');
 const { organizationUpdate } = require('../schemas/organization');
+const emptyBody = require('../middlewares/emptyBody');
 
 //GET public organization
 router.get('/public/:id', getPublicOrganization);
 
 //PATCH update public data of an organization
-router.patch('/public/:id', isAdmin, schemaValidator(organizationUpdate), updatePublicOrganization);
+router.patch(
+  '/public/:id',
+  isAdmin,
+  schemaValidator(organizationUpdate),
+  emptyBody.organization,
+  updatePublicOrganization
+);
 
 module.exports = router;
