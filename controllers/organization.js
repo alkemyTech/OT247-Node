8ad0efa2049c7f81d5cf3d811b5c92370f014940
Nicolas Preiti <nicolaspreiti45@gmail.com>
2022-07-26 03:29:27 -0300

    validations inside controller removed

--- controllers/organization.js
+++ controllers/organization.js
@@ -18,24 +18,17 @@ module.exports = {
   updatePublicOrganization: async (req, res) => {
     try {
       const { id } = req.params;
-
-      //Check that param id is a integer
-      if (!Number.isInteger(Number(id)))
-        return res.status(412).json({ status: 412, message: 'Id param must be a integer' });
-
-      //Check that req.body is not empty
-      if (Object.keys(req.body) == 0)
-        return res.status(412).json({ status: 412, message: 'Empty fields, nothing to update' });
+      const { body } = req;
 
       //Try to update an organization
-      const updatedOrganization = await updateOrganization(id, req.body);
+      const organization = await updateOrganization(id, body);
 
       //Server Responses
-      updatedOrganization == 0
+      !Number(organization)
         ? res.status(404).json({ status: 404, message: 'Organization not found' })
         : res.status(200).json({ status: 200, message: 'Organization data updated' });
     } catch (err) {
-      res.status(400).json({ status: 400, error: 'An error has occurred' });
+      res.status(400).json({ status: 400, error: 'An error has occurred', error: err.message });
     }
   },
 };
