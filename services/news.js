0b3704b9ff6b94e820f7e731bd1614186aa92a1b
Rodrigo Conejeros <rconejerosnavea@gmail.com>
2022-07-27 17:12:52 -0400

    Service to validate if news exists and delete it

---
+++ services/news.js
@@ -0,0 +1,17 @@
+const { ErrorObject } = require('../helpers/error')
+const { News } = require('../models')
+const { existNews } = require('../helpers/existNews')
+
+
+const destroyNews = async (id) => {
+  try{
+  	const news = await existNews(id)
+    if(!news) throw new ErrorObject('News not found', 404)
+
+    return await News.destroy({ where: { id } })
+  }catch(error){
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}
+
+module.exports = { destroyNews }
