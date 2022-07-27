2054e9dc9e953c5abaf0d754e7fcf03f79bf233c
Rodrigo Conejeros <rconejerosnavea@gmail.com>
2022-07-26 21:03:08 -0400

    Added controller to use service to delete activities

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,3 +1,26 @@
-const { Activities } = require('../models');
+const createHttpError = require('http-errors')
+const { endpointResponse } = require('../helpers/success')
+const { parseIntId } = require('../helpers/parseIntId')
 
-module.exports = Activities;
+const { destroyActivity } = require('../services/activities')
+
+module.exports = {
+  deleteActivity: async (req, res, next) => {
+    try {
+      const { id } = req.params
+
+      const deletedActivity = await destroyActivity(parseIntId(id))
+      endpointResponse({
+          res,
+          message: 'Activity deleted successfully',
+          body: deletedActivity,
+      })
+    }catch (error) {
+      const httpError = createHttpError(
+          error.statusCode,
+          `[Error deleting activity] - [activities - DELETE]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }
+} 
