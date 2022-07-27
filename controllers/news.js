1d231d33b591151479689fa9c8097d58fb8ed303
Rodrigo Conejeros <rconejerosnavea@gmail.com>
2022-07-27 17:10:00 -0400

    Controller to delete news

---
+++ controllers/news.js
@@ -0,0 +1,26 @@
+const createHttpError = require('http-errors')
+const { endpointResponse } = require('../helpers/success')
+const { parseIntId } = require('../helpers/parseIntId')
+
+const { destroyNews } = require('../services/news')
+
+module.exports = {
+  deleteNews: async (req, res, next) => {
+    try {
+      const { id } = req.params
+
+      const deletedNews = await destroyNews(parseIntId(id))
+      endpointResponse({
+          res,
+          message: 'News deleted successfully',
+          body: deletedNews,
+      })
+    }catch (error) {
+      const httpError = createHttpError(
+          error.statusCode,
+          error.message
+      )
+      next(httpError)
+    }
+  }
+}
