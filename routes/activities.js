73ac35bf4879c9f3e435b5f63d3129a7b8e3031d
rodrigoacn <107000658+rodrigoacn@users.noreply.github.com>
2022-07-27 17:30:42 -0400

    Don't comes to case

--- routes/activities.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const { activitiesRegister } = require('../controllers/activities');
-
-router.post('/', isAdmin, function(req, res, next){
-  activitiesRegister(req, res, next);
-});
-
-module.exports = router;
