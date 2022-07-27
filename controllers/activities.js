e2a7166933951cf0bf5c39b33befd0bc3b021c61
Rodrigo Conejeros <rconejerosnavea@gmail.com>
2022-07-27 17:45:42 -0400

    Fixed

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,26 +1,3 @@
-const createHttpError = require('http-errors')
-const { endpointResponse } = require('../helpers/success')
-const { catchAsync } = require('../helpers/catchAsync')
+const { Activities } = require('../models');
 
-const { insertActivity } = require('../services/activity')
-
-module.exports = {
-  activitiesRegister: catchAsync(async (req, res, next) => {
-    try {
-      const { body } = req
-
-      const activity = await insertActivity(body)
-      endpointResponse({
-        res,
-        message: 'Activity created successfully',
-        body: activity,
-      }) 
-    } catch (error) {
-      const httpError = createHttpError(
-        error.statusCode,
-        `[Error creating activity] - [activities - POST]: ${error.message}`,
-      );
-      next(httpError) 
-    }
-  })
-}
+module.exports = Activities;
