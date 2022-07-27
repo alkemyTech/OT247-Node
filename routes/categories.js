18d177e0d657ad64e4588eeaae8b7a6ab2f888af
Rodrigo Tonso <67134489+rodrigotonso@users.noreply.github.com>
2022-07-27 17:45:04 -0300

    fix categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { getCategoriesNames } = require('../controllers/categories');
 const { isAdmin } = require('../middlewares/isAdmin')
 const { verify } = require('../middlewares/verifyToken')
 const categoryCtrl = require('../controllers/categories');
@@ -9,7 +8,7 @@ const { category } = require('../schemas/category')
 const { categoryExists } = require('../middlewares/categoryExists')
 
 router
-  .get('/', isAdmin, getCategoriesNames)
+  .get('/', isAdmin, categoryCtrl.getCategoriesNames)
   .post('/', verify, isAdmin, schemaValidator(category), categoryCtrl.createCategory)
   .delete('/:id', isAdmin, categoryExists, categoryCtrl.deleteCategoryById)
 
