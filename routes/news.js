9ff150ba337c3d64d5046c9271c97e51efbbc840
Rodrigo Conejeros <rconejerosnavea@gmail.com>
2022-07-27 16:58:18 -0400

    Route to delete news /:id

---
+++ routes/news.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const { isAdmin } = require('../middlewares/isAdmin')
+const { deleteNews } = require('../controllers/news')
+
+router.delete('/:id', isAdmin, deleteNews)
+
+module.exports = router
