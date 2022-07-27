41d2d48d1697ef271ffe3cbf79b6e83b3e469a3b
Rodrigo Conejeros <rconejerosnavea@gmail.com>
2022-07-26 21:13:29 -0400

    Added activities delete route

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 const categoriesRouter = require('./categories');
 const organizationsRouter = require('./organizations');
+const activitiesRouter = require('./activities')
 const authRouter = require('./auth');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' })
 });
 
 //Auth
 router.use('/auth', authRouter)
 
 //Categories
-router.use('/categories', categoriesRouter);
+router.use('/categories', categoriesRouter)
 
 //Organizations
-router.use('/organizations', organizationsRouter);
+router.use('/organizations', organizationsRouter)
 
+//Activities
+router.use('/activities', activitiesRouter)
+
+//Activities
 module.exports = router;
