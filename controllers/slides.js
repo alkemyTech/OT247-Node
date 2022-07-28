c545f39927bcad035bbfbc9be03fec60e8efbd2b
Rodrigo Conejeros <rconejerosnavea@gmail.com>
2022-07-28 18:23:39 -0400

    Added controller to list slides

---
+++ controllers/slides.js
@@ -0,0 +1,12 @@
+const { getSlidesService } = require('../services/slides');
+
+module.exports = {
+	getSlides: async (req, res) => {
+    try {
+      const slides = await getSlidesService();
+      res.status(200).json(slides);
+    } catch (error) {
+      res.status(400).send('an error has occurred');
+    }
+  }
+};
