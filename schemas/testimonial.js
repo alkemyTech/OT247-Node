32ae0dc63df2654d1c5469cab8d0ac5c37f4fad1
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2022-07-29 21:07:00 -0300

    Add: testimoniaÃl Schema

---
+++ schemas/testimonial.js
@@ -0,0 +1,35 @@
+exports.testimonial = {
+    name: {
+        exists: {
+            errorMessage: 'There must be a name',
+        },
+        notEmpty: {
+            errorMessage: 'Name is empty',
+        },
+        isString: {
+            errorMessage: 'The name must be a string',
+        },
+    },
+    image: {
+        exists: {
+            errorMessage: 'There must be an image',
+        },
+        notEmpty: {
+            errorMessage: 'Image is empty',
+        },
+        isString: {
+            errorMessage: 'The image must be a string',
+        },
+    },
+    content: {
+        exists: {
+            errorMessage: 'There must be a content',
+        },
+        notEmpty: {
+            errorMessage: 'Content is empty',
+        },
+        isString: {
+            errorMessage: 'The content must be a string',
+        },
+    },
+};
