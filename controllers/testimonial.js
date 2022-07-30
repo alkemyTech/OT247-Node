e4ea82e2e9da3ec831399be8687bdb6d5664a627
Rodrigo Conejeros <rconejerosnavea@gmail.com>
2022-07-30 19:42:38 -0400

    Added controller to delete testimonials

--- controllers/testimonial.js
+++ controllers/testimonial.js
@@ -1,3 +1,25 @@
-const { Testimonial } = require('../models');
+const createHttpError = require('http-errors');
+const { endpointResponse } = require('../helpers/success');
 
-module.exports = Testimonial;
+const { deleteTestimonialService } = require('../services/testimonial');
+
+module.exports = {
+  deleteTestimonial: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+
+      const deletedTestimonial = await deleteTestimonialService(parseInt(id, 10));
+      endpointResponse({
+        res,
+        message: 'Testimonial deleted successfully',
+        body: deletedTestimonial,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        error.message,
+      );
+      next(httpError);
+    }
+  },
+};
