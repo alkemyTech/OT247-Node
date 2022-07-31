ce647485e5a9fa97437eff793ec5f075bc7ab37b
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2022-07-31 03:31:35 -0300

    Add GET route for Members

---
+++ routes/members.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const { getMembers } = require('../controllers/members');
+const { verify } = require('../middlewares/verifyToken');
+const { schemaValidator } = require('../middlewares/validator');
+const { member } = require('../schemas/member');
+const { isAdmin } = require('../middlewares/isAdmin');
+
+router.get('/', verify, isAdmin, getMembers);
+
+module.exports = router;
