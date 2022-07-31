735dc268ec7edc1ca1e3a6715dcdf805f0d3c7d7
Nicolas Preiti <nicolaspreiti45@gmail.com>
2022-07-31 12:57:13 -0300

    create member controller created

--- controllers/members.js
+++ controllers/members.js
@@ -1,6 +1,23 @@
-const { Member } = require('../models');
+const memberService = require('../services/member');
+const { endpointResponse } = require('../helpers/success');
+const { ErrorObject } = require('../helpers/error');
 
-// Example instead nothing
-const membersController = (req, res) => {};
+module.exports = {
+  createMember: async (req, res) => {
+    try {
+      const { body } = req;
 
-module.exports = { membersController };
+      // Create a member
+      const member = await memberService.createMember(body);
+
+      return endpointResponse({
+        res,
+        message: 'Member created successfully',
+        body: member,
+      });
+    } catch (err) {
+      const error = new ErrorObject(err.message, err.statusCode || 400, err.errors || err.stack);
+      return res.json(error);
+    }
+  },
+};
