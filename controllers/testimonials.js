4937a1c1326982d637c08b624f275b4029a2cf6a
Alexis Correa <alexis.correa026@gmail.com>
2022-08-01 01:00:13 -0300

    Response logic when updating testimony

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -1,14 +1,14 @@
 const createHttpError = require('http-errors');
 const { endpointResponse } = require('../helpers/success');
 const { catchAsync } = require('../helpers/catchAsync');
-const { createTestimonialsService } = require('../services/testimonial');
+const { createTestimonialsService, updateTestimonial } = require('../services/testimonial');
 
 module.exports = {
   createTestimonial: catchAsync(async (req, res, next) => {
     try {
       const { name, image, content } = req.body;
 
-      const newTestimonial = { name, image, content };        
+      const newTestimonial = { name, image, content };
       const createdTestimonial = await createTestimonialsService(newTestimonial);
 
       endpointResponse({
@@ -16,12 +16,33 @@ module.exports = {
         message: 'Testimonial created successfully',
         body: createdTestimonial,
       });
-      } catch (err) {
-        const httpError = createHttpError(
-          error.statusCode,
-          `[Error updating activity] - [activities - PUT]: ${error.message}`,
-        );
-        next(httpError);
-      }
-  }),   
-}
+    } catch (err) {
+      const httpError = createHttpError(
+        err.statusCode,
+        `[Error updating activity] - [activities - PUT]: ${err.message}`,
+      );
+      next(httpError);
+    }
+  }),
+  updateTestimonial: catchAsync(async (req, res, next) => {
+    try {
+      const { body } = req;
+      const { id } = req.params;
+
+      const testimonial = await updateTestimonial(id, body);
+      endpointResponse({
+        code: 200,
+        res,
+        body: testimonial,
+        message: 'Testimonial successfully updated',
+      });
+    } catch (err) {
+      const httpError = createHttpError(
+        err.statusCode,
+        `[Error updating activity] - [activities - PUT]: ${err.message}`,
+      );
+      next(httpError);
+    }
+  }),
+
+};
