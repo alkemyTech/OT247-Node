6ced4c4b55ebbff7f893f9be23da5989cbe335ec
Alexis Correa <alexis.correa026@gmail.com>
2022-07-31 23:56:04 -0300

    Created endpoint/route testimonial

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,13 +1,14 @@
 const express = require('express');
+
 const router = express.Router();
 
 const { isAdmin } = require('../middlewares/isAdmin');
 const { verify } = require('../middlewares/verifyToken');
-const { createTestimonial } = require('../controllers/testimonials');
+const { createTestimonial, updateTestimonial } = require('../controllers/testimonials');
 const { schemaValidator } = require('../middlewares/validator');
 const { testimonial } = require('../schemas/testimonial');
 
-router
-  .post('/', verify, isAdmin, schemaValidator(testimonial), createTestimonial);
+router.post('/', verify, isAdmin, schemaValidator(testimonial), createTestimonial);
+router.put('/:id', verify, isAdmin, schemaValidator(testimonial), updateTestimonial);
 
 module.exports = router;
