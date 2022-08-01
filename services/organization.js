918ceb74fe350c50738d4addb3994399559a33c2
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-08-01 14:11:28 -0300

    Add: add slides in query

--- services/organization.js
+++ services/organization.js
@@ -1,10 +1,16 @@
 const { Organization } = require('../models');
+const { Slide } = require('../models');
 
 const getPublicOrgService = async (id) => {
   try {
     return await Organization.findOne({
       attributes: ['name', 'image', 'phone', 'address'],
       where: { id },
+      include: {
+        model: Slide,
+        attributes: ['imageUrl', 'text', 'order'],
+      },
+      order: [[Slide, 'order', 'ASC']],
     });
   } catch (err) {
     return { error: err };
