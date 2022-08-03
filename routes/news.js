2ca0d4388d25bf745ff4851555886ce6f85141b9
Nicolas Preiti <nicolaspreiti45@gmail.com>
2022-08-03 08:45:46 -0300

    fixes in news route

--- routes/news.js
+++ routes/news.js
@@ -1,15 +1,19 @@
 const express = require('express');
 
 const router = express.Router();
-const { createNews, updateNews, getNewsById, deleteNews } = require('../controllers/news');
+const {
+  createNews, updateNews, getNewsById, deleteNews,
+} = require('../controllers/news');
 const { isAdmin } = require('../middlewares/isAdmin');
 const { schemaValidator } = require('../middlewares/validator.js');
-const { news } = require('../schemas/news');
 const { existNews } = require('../helpers/existNews');
 const { newsCreate } = require('../schemas/news');
 
+// Este esquema no existe, deberia crearse
+// const { news } = require('../schemas/news');
+
 router.get('/:id', isAdmin, getNewsById);
-router.put('/:id', isAdmin, existNews, schemaValidator(news), updateNews);
+router.put('/:id', isAdmin, existNews, updateNews);
 router.post('/', isAdmin, schemaValidator(newsCreate), createNews);
 router.delete('/:id', isAdmin, deleteNews);
 
