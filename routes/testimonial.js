1d85be2163d263bf8461f8139258d024b75cf1e5
rodrigoacn <107000658+rodrigoacn@users.noreply.github.com>
2022-08-04 11:01:18 -0400

    Changed name

--- routes/testimonial.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const { isAdmin } = require('../middlewares/isAdmin');
-const { deleteTestimonial } = require('../controllers/testimonial');
-
-router.delete('/:id', isAdmin, deleteTestimonial);
-
-module.exports = router;
