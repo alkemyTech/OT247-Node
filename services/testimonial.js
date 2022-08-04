3065655d9e8c24d8f2adfc48572e1fa6dfbe2592
rodrigoacn <107000658+rodrigoacn@users.noreply.github.com>
2022-08-04 10:27:16 -0400

    Fixed

--- services/testimonial.js
+++ services/testimonial.js
@@ -1,55 +1,25 @@
-const { Testimonial } = require('../models');
-const { ErrorObject } = require('../helpers/error');
-const { existTestimonial } = require('../helpers/existTestimonial');
+const createHttpError = require('http-errors');
+const { endpointResponse } = require('../helpers/success');
 
-const createTestimonialsService = async (body) => {
-  try {
-    const createTestimonial = await Testimonial.create(
-      {
-        name: body.name,
-        image: body.image,
-        content: body.content,
-      },
-    );
-    return createTestimonial;
-  } catch (error) {
-    throw new ErrorObject(404, 'News not found');
-  }
-};
-
-const updateTestimonialService = async (id, body) => {
-  try {
-    const testimonial = await Testimonial.findOne({
-      where: { id },
-    });
-    if (!testimonial) {
-      throw new ErrorObject('Testimonial not found', 404);
-    }
-    await Testimonial.update({
-      name: body.name,
-      image: body.image,
-      content: body.content,
-    }, {
-      where: { id },
-    });
-    return testimonial;
-  } catch (error) {
-    throw new ErrorObject(error.message, error.statusCode || 500);
-  }
-};
-
-const deleteTestimonialService = async (id) => {
-  try {
-    const testimonial = await existTestimonial(id);
-    if (!testimonial) throw new ErrorObject('Testimonial not found', 404);
-    return await Testimonial.destroy({ where: { id } });
-  } catch (error) {
-    throw new ErrorObject(error.message, error.statusCode || 500);
-  }
-};
+const { deleteTestimonialService } = require('../services/testimonial');
 
 module.exports = {
-  createTestimonialsService,
-  updateTestimonialService,
-  deleteTestimonialService,
+  deleteTestimonial: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+
+      const deletedTestimonial = await deleteTestimonialService(parseInt(id, 10));
+      endpointResponse({
+        res,
+        message: 'Testimonial deleted successfully',
+        body: deletedTestimonial,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        error.message,
+      );
+      next(httpError);
+    }
+  },
 };
