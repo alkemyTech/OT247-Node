4704fc96d1e009a3e6ec06fc2a7fc09ab81684db
Nicolas Preiti <nicolaspreiti45@gmail.com>
2022-08-05 11:50:12 -0300

    updateSlide route created

--- routes/slides.js
+++ routes/slides.js
@@ -4,13 +4,16 @@ const router = express.Router();
 const { getSlides } = require('../controllers/slides');
 const { isAdmin } = require('../middlewares/isAdmin');
 const { deleteSlideById } = require('../controllers/slides');
-const { getSlideById } = require('../controllers/slides');
+const { getSlideById, updateSlideById } = require('../controllers/slides');
 const { verify } = require('../middlewares/verifyToken');
 const { slideExists } = require('../middlewares/slideExists');
+const slideSchema = require('../schemas/slide');
+const { schemaValidator } = require('../middlewares/validator');
 
 router.get('/', isAdmin, getSlides);
-// Delete slide
 router.delete('/:id', verify, isAdmin, slideExists, deleteSlideById);
 router.get('/:id', verify, isAdmin, slideExists, getSlideById);
+
+router.put('/:id', verify, isAdmin, schemaValidator(slideSchema), updateSlideById);
 
 module.exports = router;
