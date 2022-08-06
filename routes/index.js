de4efc2f9c523e624a9aca08866bc316b9ea6120
Nicolas Preiti <nicolaspreiti45@gmail.com>
2022-08-05 22:43:41 -0300

    index routes refactor

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,27 @@
 const express = require('express');
+const { importAllJS } = require('../helpers/importAll');
+const { verify } = require('../middlewares/verifyToken');
 
 const router = express.Router();
+const routes = importAllJS(__filename, __dirname);
 
-const categoriesRouter = require('./categories');
-const organizationsRouter = require('./organizations');
-const newsRouter = require('./news');
-const authRouter = require('./auth');
-const userRouter = require('./users');
-const activitiesRouter = require('./activities');
-const contactsRouter = require('./contacts');
-const slidesRouter = require('./slides');
-const testimonialsRouter = require('./testimonials');
-const membersRouter = require('./members');
-const backofficeRouter = require('./backoffice');
+// Unprotected Routes
+router
+  .use('/', routes.root)
+  .use('/auth', routes.auth);
 
-/* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
-});
-
-// Auth
-router.use('/auth', authRouter);
-
-// Categories
-router.get('/login', (req, res) => {
-  res.render('login');
-});
-
-router.use('/categories', categoriesRouter);
-
-// Organizations
-router.use('/organizations', organizationsRouter);
-
-// Users
-router.use('/users', userRouter);
-
-// Activities
-router.use('/activities', activitiesRouter);
-
-// News
-router.use('/news', newsRouter);
-
-// Contacts
-router.use('/contacts', contactsRouter);
-router.use('/slides', slidesRouter);
-
-// Testimonials
-router.use('/testimonials', testimonialsRouter);
-
-// Members
-router.use('/members', membersRouter);
-
-// BackOffice
-router.use('/backoffice', backofficeRouter);
+// Protected Routes
+router
+  .use(verify)
+  .use('/activities', routes.activities)
+  .use('/backoffice', routes.backoffice)
+  .use('/categories', routes.categories)
+  .use('/contacts', routes.contacts)
+  .use('/members', routes.members)
+  .use('/news', routes.news)
+  .use('/organizations', routes.organizations)
+  .use('/slides', routes.slides)
+  .use('/testimonials', routes.testimonials)
+  .use('/users', routes.users);
 
 module.exports = router;
