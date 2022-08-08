9bd89bbe59bff97b72021eb7ff35900a426c82d8
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2022-08-08 02:10:58 -0300

    Add: isAuthorized Comment middleware

--- routes/comment.js
+++ routes/comment.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-const { isAdmin } = require('../middlewares/isAdmin');
+const { isAuthorized } = require('../middlewares/isAuthorizedComment');
 const { verify } = require('../middlewares/verifyToken');
 const { schemaValidator } = require('../middlewares/validator');
 const { comment } = require('../schemas/comment');
 const commentCtrl = require('../controllers/comments');
 const { existComment } = require('../helpers/existComment');
 
-router.put('/:id', verify, isAdmin, existComment, schemaValidator(comment), commentCtrl.updateCommentById);
+router.put('/:id', verify, isAuthorized, existComment, schemaValidator(comment), commentCtrl.updateCommentById);
 
 module.exports = router;
