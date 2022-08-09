5ce0455e4df167aaa44ce02c95b2d103a8b83802
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-08-09 20:00:01 -0300

    change: manual changes in models

--- models/news.js
+++ models/news.js
@@ -2,12 +2,7 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class News extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
+    static associate() {
     }
   }
   News.init({
