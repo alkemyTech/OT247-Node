29ecedc8b454feaf1c4619e63fc0aa4d5ca7c452
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-08-09 20:07:24 -0300

    change: manual changes in app.js

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const indexRouter = require('./routes/index');
 const app = express();
 app.use(cors());
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -23,18 +22,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
+app.use((err, req, res) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
