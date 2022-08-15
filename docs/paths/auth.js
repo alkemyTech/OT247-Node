6a9a4170c2ef556c962d87cb123d997b46269c38
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-08-15 15:14:40 -0300

    add: user docs

---
+++ docs/paths/auth.js
@@ -0,0 +1,88 @@
+const Auth = {
+  path: {
+    '/auth/login': {
+      post: {
+        tags: ['auth'],
+        summary: 'Login',
+        description: 'This endpoint is login for users',
+        requestBody: {
+          required: true,
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/UserLogin',
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description: 'user description and token',
+          },
+          400: {
+            description: 'error user login',
+          },
+          500: {
+            description: 'data and hash arguments required',
+          },
+        },
+      },
+    },
+    '/auth/register': {
+      post: {
+        tags: ['auth'],
+        summary: 'Register',
+        description: 'This endpoint is a register for users',
+        requestBody: {
+          required: true,
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/UserRegister',
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description: 'testimonial description',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/UserRegister',
+                },
+              },
+            },
+          },
+          400: {
+            description: 'Bad Request',
+          },
+        },
+      },
+    },
+    '/auth/me': {
+      get: {
+        tags: ['auth'],
+        summary: 'User info',
+        description: ' This endpoint is a user info',
+        parameters: [],
+        responses: {
+          200: {
+            description: 'user data',
+          },
+          403: {
+            description: 'Forbidden',
+          },
+          400: {
+            description: 'unexpected error',
+          },
+        },
+        security: [
+          { bearerAuth: [] },
+        ],
+      },
+    },
+  },
+};
+
+module.exports = Auth;
